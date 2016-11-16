1b6ac746d0ede691b1a6b2dee79b5fec4917df22
William Cheng <williamcheng@Williams-MacBook-Air.local>
2016-11-16 21:38:28 +0000

    Added a loading bar

--- Functions.py
+++ Functions.py
@@ -9,6 +9,7 @@ from numpy.fft import rfft
 from numpy.fft import irfft
 from sklearn.tree import export_graphviz
 import subprocess
+import sys
 
 
 ##############################################################################################################
@@ -285,3 +286,26 @@ def visualize_tree(tree, feature_names):
     except:
         exit("Could not run dot, ie graphviz, to "
              "produce visualization")
+
+
+# Print iterations progress
+def print_progress(iteration, total, prefix='', suffix='', decimals=1, barLength=100):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        barLength   - Optional  : character length of bar (Int)
+    """
+    format_str = "{0:." + str(decimals) + "f}"
+    percent = format_str.format(100 * (iteration / float(total)))
+    filled_length = int(round(barLength * iteration / float(total)))
+    bar = '#' * filled_length + '-' * (barLength - filled_length)
+    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percent, '%', suffix)),
+    if iteration == total:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
--- feature_gather.py
+++ feature_gather.py
@@ -5,6 +5,8 @@ import numpy as np
 import h5py
 from numpy.fft import rfft
 # from numpy.fft import irfft
+from time import sleep
+from Functions import print_progress
 # SingleSource_ECGdata_Itt1000_P60.h5
 
 filename = raw_input("Training data to convert into pandas dataframe: ")
@@ -21,8 +23,10 @@ columns = ['Max Value', 'Min Value', 'Minmax Diff', 'Sample Intensity', 'Sample 
                                                                  'Probe Position', 'Distance', 'Target']
 
 count = 0
+i = 0
+l = len(trainfile.keys())
 
-start_time1 = time.time()
+# start_time1 = time.time()
 for index_num, index in enumerate(trainfile.iterkeys()):
     group = trainfile.get('%s' % index)
     cp = np.array(group['Crit Position'])
@@ -30,10 +34,12 @@ for index_num, index in enumerate(trainfile.iterkeys()):
     ecg_vals = np.array(group['ECG'])
     ecg_0 = ecg_vals[0]
     fft_0 = rfft(ecg_0)
+    i += 1
+    print_progress(i, l, prefix='Progress:', suffix='Complete', barLength=50)
     for number in range(400):
         feature_grid[count] = feature_extract(number, ecg_vals=ecg_vals, cp=cp, probes=probes)
         count += 1
-print("--- Simulation: %s seconds ---" % (time.time() - start_time1))
+# print("--- Simulation: %s seconds ---" % (time.time() - start_time1))
 
 df_index = range(400000)
 df = pd.DataFrame(data=feature_grid, index=df_index, columns=columns)
