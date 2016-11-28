86a99a80c7a98beaad13104b076da0ad92e2c75d
William Cheng <williamcheng@Williams-MacBook-Air.local>
2016-11-28 11:41:45 +0000

    Save data instead on plotting it straight away.

--- feature_robust.py
+++ feature_robust.py
@@ -5,13 +5,14 @@ from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import seaborn as sns
 from Functions import print_progress
+import cPickle
 
 datafile = raw_input("Pandas dataframe to open: ")
 X = pd.read_hdf(os.path.join('Dataframes', "%s.h5" % datafile))
 y = X.pop('Target')
 y = y.astype(int)
 
-rows = 25
+rows = 2
 robustness_datagrid = np.zeros((rows, len(X.columns)))
 
 pp = 0
@@ -31,12 +32,18 @@ sorted_std_importance = np.sort(np.std(robustness_datagrid, axis=0))
 indicies = np.argsort(mean_importance)[::1]
 feature_names_sorted = [X.columns[ind] for ind in indicies]
 
-fig = sns.plt.figure()
-sns.set(style="white")
-sns.barplot(sorted_mean_importance, feature_names_sorted, xerr=sorted_std_importance, color='r',
-            error_kw={'ecolor': 'k'})
-sns.plt.title("Feature Importance for %s" % datafile, fontsize=16)
-sns.plt.xlabel("Mean Feature Importance (Gini)", fontsize=14)
-sns.despine(left=True, offset=10)
-sns.plt.show()
-sns.plt.close()
+MY_dIR = os.path.realpath(os.path.dirname(__file__))
+PICKLE_DIR = os.path.join(MY_dIR, 'Data')
+fname = os.path.join(PICKLE_DIR, '%s_robust.p' % datafile)
+with open(fname, 'wb') as f:
+    cPickle.dump((sorted_mean_importance, sorted_std_importance, feature_names_sorted), f)
+
+# fig = sns.plt.figure()
+# sns.set(style="white")
+# sns.barplot(sorted_mean_importance, feature_names_sorted, xerr=sorted_std_importance, color='r',
+#             error_kw={'ecolor': 'k'})
+# sns.plt.title("Feature Importance for %s" % datafile, fontsize=16)
+# sns.plt.xlabel("Mean Feature Importance (Gini)", fontsize=14)
+# sns.despine(left=True, offset=10)
+# sns.plt.show()
+# sns.plt.close()
