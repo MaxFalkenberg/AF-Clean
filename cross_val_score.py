72a881cce2a1b3db8d2a0d4ce2c20abf4070665b
William Cheng <williamcheng@Williams-MacBook-Air.local>
2016-12-06 12:31:37 +0000

    Removed prune methods, Not complete

--- cross_val_score.py
+++ cross_val_score.py
@@ -1,28 +1,17 @@
 import pandas as pd
 import numpy as np
 from sklearn.cross_validation import cross_val_score
-from sklearn.externals import joblib
-from Functions import feature_prune
+from sklearn.ensemble import RandomForestClassifier
 import os
 
-treefile = raw_input("ML file to load: ")
-dtree = joblib.load(os.path.join('ML_models', "%s.p" % treefile))
 datafile = raw_input("Pandas dataframe to open: ")
-X = pd.read_hdf(os.path.join('Data', "%s.h5" % datafile))
-# feature_prune(X, ['Distance', 'Crit Position', 'Probe Position',
-#                   'Unit Vector X', 'Unit Vector Y', 'Theta', 'Sample Length'])
-# feature_prune(X, ['Largest FT Mag %s' % x for x in range(1, 10)])
-# feature_prune(X, ['Largest FT Freq %s' % x for x in range(1, 10)])
-feature_prune(X, ['Sample Length', 'Crit Position 0', 'Crit Position 1', 'Probe Position', 'Distance 0', 'Distance 1',
-                  'Unit Vector X 0', 'Unit Vector X 1', 'Unit Vector Y 0',
-                  'Unit Vector Y 1', 'Theta 0', 'Theta 1', 'Target 0', 'Target 1',
-                  'Nearest Crit Position', 'True Distance'])
-feature_prune(X, ['Largest FT Mag %s' % x for x in range(1, 10)])
-feature_prune(X, ['Largest FT Freq %s' % x for x in range(1, 10)])
-y = X.pop('True Target')
+forest = RandomForestClassifier(n_estimators=3)
+X = pd.read_hdf(os.path.join('Dataframes', "%s.h5" % datafile))
+y = X.pop('Target')
 y = y.astype(int)
 
-scores = cross_val_score(dtree, X, y, cv=5, scoring='accuracy')
+scores = cross_val_score(forest, X, y, cv=2, scoring='recall', )
 
 print scores
-print np.mean(scores)
+# print np.mean(scores)
+# print np.std(scores, ddof=1)
