c4a7acf7ff71c2b12717facb7f82b8bc268ba6fd
William Cheng <williamcheng@Williams-MacBook-Air.local>
2016-12-06 12:26:24 +0000

    Removed pruning method as dataframes are already pruned.

--- cross_val.py
+++ cross_val.py
@@ -1,18 +1,16 @@
-# SingleSource_ECGdata_Itt1000_P60_df
+"""
+File that finds the score accuracy and its error with changing number of estimators
+"""
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import cross_val_score
-from Functions import print_progress, feature_prune
+from Functions import print_progress
 import os
 import cPickle
 
 datafile = raw_input("Pandas dataframe to open: ")
 X = pd.read_hdf(os.path.join('Data', "%s.h5" % datafile))
-feature_prune(X, ['Distance', 'Crit Position', 'Probe Position',
-                  'Unit Vector X', 'Unit Vector Y', 'Theta', 'Sample Length'])
-feature_prune(X, ['Largest FT Mag %s' % x for x in range(1, 10)])
-feature_prune(X, ['Largest FT Freq %s' % x for x in range(1, 10)])
 y = X.pop('Target')
 y = y.astype(int)
 
