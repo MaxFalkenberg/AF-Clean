0e546ebd4865d57052cda7a33d926672a87259be
William Cheng <williamcheng@Williams-MacBook-Air.local>
2017-01-25 14:34:05 +0000

    Added regression feature gathering, can also input number of models to create.

--- feature_robust.py
+++ feature_robust.py
@@ -6,23 +6,37 @@ import os
 import pandas as pd
 import numpy as np
 from sklearn.cross_validation import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from Functions import print_progress
 import cPickle
 
+while True:
+    input_list = ['c', 'r']
+    RFtype = raw_input("Classifier or Regressor (c/r): ")
+    if RFtype in input_list:
+        break
+
+model_number = int(raw_input("Number of models: "))
+
 datafile = raw_input("Pandas dataframe to open: ")
 X = pd.read_hdf("%s.h5" % datafile)
-y = X.pop('Target 0')
-y = y.astype(int)
+if RFtype == 'c':
+    y = X.pop('Target 0')
+    y = y.astype(int)
+if RFtype == 'r':
+    y = X.pop('Distance 0')
 
-rows = 25
+rows = model_number
 robustness_datagrid = np.zeros((rows, len(X.columns)))
 
 pp = 0
 print_progress(pp, rows, prefix='Progress:', suffix='Complete', bar_length=50)
 for row in range(rows):
     X_train, X_test, y_train, y_test = train_test_split(X, y)
-    forest = RandomForestClassifier(n_estimators=15)
+    if RFtype == 'c':
+        forest = RandomForestClassifier(n_estimators=15)
+    if RFtype == 'r':
+        forest = RandomForestRegressor(n_estimators=15)
     forest.fit(X_train, y_train)
     robustness_datagrid[row] = forest.feature_importances_
     pp += 1
