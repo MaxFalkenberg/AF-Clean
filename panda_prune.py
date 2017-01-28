a677dc15c6451d9e85020cd21715fc361490cc85
William Cheng <williamcheng@Williams-MacBook-Air.local>
2017-01-28 14:37:09 +0000

    added more features to the probe_features list.

--- panda_prune.py
+++ panda_prune.py
@@ -12,7 +12,7 @@ dataframe = pd.read_hdf("%s.h5" % datafile)
 
 print "\n"
 print "Dataframe description:"
-with open(os.path.join('Data_desc', '%s.txt' % datafile)) as f:
+with open('%s.txt' % datafile) as f:
     print f.read()
 print "\n"
 
@@ -27,7 +27,7 @@ filename = raw_input("Saved file name: ")
 # Need to Alter this to be consistent with double (or more) sources.
 probe_features = ['Crit Position', 'Crit Position 0', 'Crit Position 1', 'Probe Position',
                   'Unit Vector X', 'Unit Vector X 0', 'Unit Vector X 1', 'Unit Vector Y', 'Unit Vector Y 0',
-                  'Unit Vector Y 1', 'Theta', 'Theta 0', 'Theta 1',
+                  'Unit Vector Y 1', 'Theta', 'Theta 0', 'Theta 1', 'Vector X 0', 'Vector Y 0',
                   'Nearest Crit Position']
 
 # Deletes features from the dataframe that are in probe_features
