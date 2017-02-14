c4057b257c105f8fac0e3a7bd35db91fe60af857
William Cheng <williamcheng@Williams-MacBook-Air.local>
2017-02-14 15:51:10 +0000

    Added Nu to the prune list.

--- panda_prune.py
+++ panda_prune.py
@@ -28,7 +28,7 @@ filename = raw_input("Saved file name: ")
 probe_features = ['Crit Position', 'Crit Position 0', 'Crit Position 1', 'Probe Position',
                   'Unit Vector X', 'Unit Vector X 0', 'Unit Vector X 1', 'Unit Vector Y', 'Unit Vector Y 0',
                   'Unit Vector Y 1', 'Theta', 'Theta 0', 'Theta 1', 'Vector X 0', 'Vector Y 0',
-                  'Nearest Crit Position', 'index']
+                  'Nearest Crit Position', 'index', 'Nu']
 
 # Deletes features from the dataframe that are in probe_features
 all_features = list(dataframe.columns)
