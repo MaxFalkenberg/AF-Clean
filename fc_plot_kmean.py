6539544ff4f9e5de798153a2b439b79d8e7cd7f3
William Cheng <williamcheng@Williams-MacBook-Air.local>
2017-02-15 20:33:53 +0000

    n_clusters is now manually entered and replaced binplot with modeplot.

--- fc_plot_kmean.py
+++ fc_plot_kmean.py
@@ -8,13 +8,13 @@ import matplotlib.cm as cm
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import scale
 from collections import Counter
-from Functions import binplot
+from Functions import modeplot
 
 # number of k-means clusters to form:
 # Off the critical circuit
 # On the critical circuit
 # At a boundry point
-n_clusters = 3
+n_clusters = int(raw_input("Number of Clusters: "))
 
 filename = raw_input("Pandas dataframe to open: ")
 bin_datafile = raw_input("Original Pandas dataframe to open: ")
@@ -28,11 +28,11 @@ k_model = KMeans(n_clusters=n_clusters, init='k-means++')
 cluster_labels = k_model.fit_predict(X)
 B['Cluster Label'] = pd.Series(cluster_labels)
 
-binned_grid, clim, feature = binplot(B, 'Cluster Label')
+binned_grid, clim, feature = modeplot(B, 'Cluster Label')
 plt.figure()
-cm = plt.cm.get_cmap('brg')
+cm = plt.cm.get_cmap('viridis')
 plt.imshow(binned_grid, vmin=clim[0], vmax=clim[1], interpolation="nearest", origin="lower", cmap=cm)
-plt.colorbar(shrink=0.4, pad=0.07)
+# plt.colorbar(shrink=0.4, pad=0.07)
 plt.xlabel('x', fontsize=18)
 plt.ylabel('y', fontsize=18)
 plt.title(feature, fontsize=18)
