added sign parameter to feature_compile. Renamed x_class to be more consistent. Fixed some PEP8 conventions

pyqt_locator.py
@@ -15,7 +15,7 @@ import propagate_singlesource as ps
 y_regress = joblib.load('y_regress_rt_4.pkl')
 y_estimator = joblib.load('y_class_rt_1.pkl')
 x_regress = joblib.load('x_regress_rt_2.pkl')
-x_class = joblib.load('x_classifier_rt_1.pkl')
+x_class = joblib.load('x_class_rt_1.pkl')
 #####################################
 
 # Initialising the Heart structure
@@ -36,14 +36,14 @@ print "Initial ECG Probe position: (%s, %s)" % (current_ecg_x_pos, current_ecg_y
 
 # Initialising the animation window
 app = QtGui.QApplication([])
-win = pg.GraphicsWindow()
+win = pg.GraphicsWindow(border=True)
 win.show()
 win.setWindowTitle('animation')
 w1 = win.addLayout()
 view = w1.addViewBox()
-img = pg.ImageItem(border='w')
+img = pg.ImageItem()
 img.setLevels([0, 50])
-label = pg.LabelItem(justify='right', border=True)
+label = pg.LabelItem(justify='right', border='w')
 win.addItem(label)
 # label = pg.TextItem()
 # view.hideAxis('left')
@@ -106,19 +106,20 @@ State 0 - always measure and process ECG
 y_regress_treshold = 3
 
 
-def rt_ecg_gathering(process_list):
+def rt_ecg_gathering(ecg_list, sign_feature=False):
     """
     Records the ECGS, Gathers the features and compiles them.
-    :param process_list: Raw data from animation_grid (t, (x,y))
+    :param ecg_list: Raw data from animation_grid (t, (x,y))
+    :param sign_feature: Flag to find sign features.
     :return: (441,) array of feature data.
     """
-    voltages = ecg_processing.solve(np.array(process_list).astype('float32'))
+    voltages = ecg_processing.solve(np.array(ecg_list).astype('float32'))
 
     # Putting all 9 ecg features into (9,21) array
     uncompiled_features = []
     for i in range(9):
         uncompiled_features.append(feature_extract_multi_test_rt(i, voltages))
-    compiled_features = multi_feature_compile_rt(np.array(uncompiled_features))
+    compiled_features = multi_feature_compile_rt(np.array(uncompiled_features), sign=sign_feature)
     return compiled_features
 
 
@@ -151,7 +152,7 @@ def update_data():
                 # ECG Recording and feature gathering
                 sample = rt_ecg_gathering(process_list)
                 # Get deprication warning if this is not done.
-                sample = sample.reshape(1,-1)
+                sample = sample.reshape(1, -1)
 
                 y_class_value = y_estimator.predict(sample)[0]
                 print "Y classification: %s" % y_class_value
@@ -170,7 +171,7 @@ def update_data():
                     if x_class_value == 1:
                         print "Found the rotor!"
                         print "Predicted Rotor position: (%s, %s)" % (current_ecg_x_pos, current_ecg_y_pos)
-                        #reseting the process.
+                        # reseting the process.
                         current_ecg_y_pos = randint(3, 196)
                         current_ecg_x_pos = randint(3, 196)
                         state = 0
@@ -188,7 +189,6 @@ def update_data():
                         del y_short_memory
                         y_short_memory = []
                         current_ecg_y_pos = loop_average
-                        # current_ecg_x_pos =
 
             if state == 1:
                 # ECG Recording and feature gathering
@@ -223,7 +223,6 @@ def update_data():
                         del x_short_memory
                         x_short_memory = []
                         current_ecg_x_pos = loop_average
-                        # current_ecg_x_pos =
 
             print "New ECG Probe position: (%s, %s)" % (current_ecg_x_pos, current_ecg_y_pos)
             print '\n'