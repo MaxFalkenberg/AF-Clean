a36fda93acddae9f08e6823fd241035a02314309
MaxFalkenberg <mff113@ic.ac.uk>
2017-03-04 11:43:05 +0000

    Function change to tree_forming

--- tree_forming.py
+++ tree_forming.py
@@ -46,10 +46,11 @@ if model_choice == 'nu':
 # label = ['Target 0', 't1.0', 't1.5', 't2.0', 't2.5', 't3.0',
 #        't3.5', 't4.0', 't4.5', 't5.0', 't5.5','t6.0', 't6.5', 't7.0',
 #        't7.5', 't8.0', 't8.5', 't9.0', 't9.5']
-label = ['X Axis True']
+label = ['Vector X 0']
 Z = X.copy()
 for i in label:
     X = Z.copy()
+    X = X[np.absolute(np.array(X['Vector Y 0'])) < 3]
     # probe_features = ['Y Axis True 3','Y Axis True 4','Y Axis True 5', 'Y Axis True 6','Y Axis True 7','Y Axis True 8','Y Axis True 9','Y Axis True 10','Y Axis True 11','Y Axis True 12','Crit Position', 'Crit Position 0', 'Crit Position 1', 'Probe Position','Target 0',
     #                   'Unit Vector X', 'Unit Vector X 0', 'Unit Vector X 1', 'Unit Vector Y', 'Unit Vector Y 0',
     #                   'Unit Vector Y 1', 'Theta', 'Theta 0', 'Theta 1', 'Probe Number', 'Multi Target 0',
@@ -58,7 +59,7 @@ for i in label:
     probe_features = ['Crit Position', 'Crit Position 0', 'Crit Position 1', 'Probe Position','Target 0',
                       'Unit Vector X', 'Unit Vector X 0', 'Unit Vector X 1', 'Unit Vector Y', 'Unit Vector Y 0',
                       'Unit Vector Y 1', 'Theta', 'Theta 0', 'Theta 1', 'Probe Number', 'Multi Target 0',
-                      'Nearest Crit Position','Vector X 0','Vector Y 0','Sample Length','Nu', 'Distance 0']
+                      'Nearest Crit Position','Sample Length','Nu', 'Distance 0']
 
     # Deletes features from the dataframe that are in probe_features
     if model_choice == 'c':
@@ -72,7 +73,7 @@ for i in label:
         # feature_prune(X, ['Distance 0'])
         #print X.keys()
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
-        X_test_meta = X_test[['Target 0', 'Vector X 0','Vector Y 0', 'Multi Target 0', 'Nu', 'Theta 0', 'Distance 0']]
+        X_test_meta = X_test[['Target 0', 'Multi Target 0', 'Nu', 'Theta 0', 'Distance 0']]
 
         all_features = list(X_train.columns)
         for feature in probe_features:
