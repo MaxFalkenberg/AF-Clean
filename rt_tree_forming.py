81c4c2ba8ef2226184a31d84dc5762f9050bf329
MaxFalkenberg <mff113@ic.ac.uk>
2017-03-05 14:26:23 +0000

    Adapted locator functions for single circuit

--- rt_tree_forming.py
+++ rt_tree_forming.py
@@ -20,7 +20,7 @@ if model_choice == 'r':
     from sklearn.ensemble import RandomForestRegressor
     if target == 'y':
         # Prunes all the probe features not to do with Y vector
-        probe_features = ['index', 'Target 0', 'Multi Target 0', 'Vector X 0', 'Theta 0', 'Distance 0', 'Nu']
+        probe_features = ['index', 'Target 0', 'Multi Target 0', 'Vector X 0', 'Theta 0', 'Distance 0', 'Nu', 'X Axis True', 'Y Axis True', 'Circuit True']
         all_features = X.columns
         for feature in probe_features:
             if feature in all_features:
@@ -30,8 +30,8 @@ if model_choice == 'r':
 
     if target == 'x':
         # Prunes all the probe features not to do with Y vector
-        X = X[np.abs(X['Vector Y 0']) < 4]
-        probe_features = ['index', 'Target 0', 'Multi Target 0', 'Vector Y 0', 'Theta 0', 'Distance 0', 'Nu']
+        X = X[np.array(X['X Axis True'])]
+        probe_features = ['index', 'Target 0', 'Multi Target 0', 'Vector Y 0', 'Theta 0', 'Distance 0', 'Nu','X Axis True', 'Y Axis True', 'Circuit True']
         all_features = X.columns
         for feature in probe_features:
             if feature in all_features:
@@ -49,13 +49,14 @@ if model_choice == 'c':
     output_choice = raw_input("Predict or Probabilities (pre\pro): ")
     from sklearn.ensemble import RandomForestClassifier
     if target == 'y':
-        probe_features = ['index', 'Target 0', 'Multi Target 0', 'Vector X 0', 'Theta 0', 'Distance 0', 'Nu']
+        X = X[np.array(X['X Axis True'])]
+        probe_features = ['index', 'Target 0', 'Multi Target 0', 'Vector X 0','X Axis True','Vector Y 0', 'Theta 0', 'Distance 0', 'Nu', 'Y Axis True']
         all_features = X.columns
         for feature in probe_features:
             if feature in all_features:
                 del X['%s' % feature]
 
-        y = X.pop('Vector Y 0')
+        y = X.pop('Circuit True')
         # y = y.apply(y_vector_classifier)
         y = y.astype(int)
 
