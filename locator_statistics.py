326c44e674adc14a9169bd2ddafc18ec4611c70f
William Cheng <williamcheng@Williams-MacBook-Air.local>
2017-03-10 01:22:15 +0000

    Baseline for statistics recorded by sign locator.

--- locator_statistics.py
+++ locator_statistics.py
@@ -16,93 +16,79 @@ with open('%s.p' % rotor_datafile, 'rb') as f:
 print data['Machine Learning Models']
 
 ECG_start = data['ECG Start']  # (x, y)
-ECG_end = data['ECG End']  # (x, y)
+ECG_end = data['ECG End']  # (x, y), y loop - ('NA', 'Y LOOP'), X loop - ('X Loop', y)
 rotor_position = data['Rotor Position']  # (x, y)
 ECG_count = data['ECG Counter']  # Total
-ECG_check = data['Check']
+Constraint_check = data['Constraint Check']  # either 0, 1, 3
+Zero_check = data['Zero Check']  # either 0, 1, 3
 
-zipped_data = zip(ECG_start, ECG_end, rotor_position, ECG_count, ECG_check)
-print zipped_data
+zipped_data = zip(ECG_start, ECG_end, rotor_position, ECG_count, Constraint_check, Zero_check)
 
-yLoop_number = ECG_end.count('Y LOOP')
+yLoop_number = ECG_end.count(('NA', 'Y LOOP'))
 xLoop_number = [x[0] for x in ECG_end].count('X LOOP')
-print yLoop_number
-print xLoop_number
 
-# yLoop_number = ECG_end.count('Y LOOP')
-# xLoop_number = ECG_end.count('X LOOP')
-#
-# succesful_locations = [x for x in zipped_data if type(x[1]) is tuple]
-# unsuccesful_locations = [x for x in zipped_data if type(x[1]) is not tuple]
-#
-# # Number of ECG statistics
-# total_ecg_average = np.mean([sum(x[3]) for x in succesful_locations])
-# total_ecg_std = np.std([sum(x[3]) for x in succesful_locations], ddof=1)
-#
-# # Distance between rotor and final ecg position
-# def distance(rotor, ecg):
-#     """
-#     :param rotor: tuple
-#     :param ecg: tuple
-#     :return:
-#     """
-#     x_vector, y_vector = vector_distance(rotor, ecg)
-#     return np.sqrt(y_vector**2 + x_vector**2)
-#
-#
-# def vector_distance(rotor, ecg):
-#     """
-#     :param rotor:
-#     :param ecg:
-#     :return:
-#     """
-#     x_vector = ecg[0] - rotor[0]
-#     y_vector = ecg[1] - rotor[1]
-#     if y_vector > 100:
-#         y_vector -= 200
-#     if y_vector <= -100:
-#         y_vector += 200
-#
-#     return x_vector, y_vector
-#
-#
-# distances = [distance(x[2], x[1]) for x in zipped_data if type(x[1]) is tuple]
-# end_vector_dis = [vector_distance(x[2], x[1]) for x in zipped_data if type(x[1]) is tuple]
-# end_abs_x_dis = [np.abs(x[0]) for x in end_vector_dis]
-# end_abs_y_dis = [np.abs(x[1]) for x in end_vector_dis]
-# start_vector_dis = [vector_distance(x[2], x[0]) for x in zipped_data if type(x[1]) is tuple]
-# start_x_dis = [x[0] for x in start_vector_dis]
-# start_y_dis = [x[1] for x in start_vector_dis]
-#
-# # distance_filterd = [x for x in distances if x<100]
-# # test = [x for x in distances if x>100]
-# # print len(distance_filterd)
-# # print np.mean(distance_filterd)
-# # print np.std(distance_filterd)
-#
-# print '{:.2f}% of rotors found.'.format(len(succesful_locations)/float(len(ECG_start)) * 100)
-# print '{:.2f}% Y Loops.'.format(yLoop_number/float(len(ECG_start)) * 100)
-# print '{:.2f}% X Loops.'.format(xLoop_number/float(len(ECG_start)) * 100)
-#
-# print "Average total number of ECGS to find rotor: {:.4f} ".format(total_ecg_average) \
-#       + u"\u00B1" + " {:.4f}".format(total_ecg_std)
-# print "Average number of ECGS to find X-Axis: {:.4f} ".format(y_ecg_average) \
-#       + u"\u00B1" + " {:.4f}".format(y_ecg_std)
-# print "Average number of ECGS to find Y-Axis: {:.4f} ".format(x_ecg_average) \
-#       + u"\u00B1" + " {:.4f}".format(x_ecg_std)
-# print "Mean distance from rotor: {:.4f} ".format(np.mean(distances)) \
-#       + u"\u00B1" + " {:.4f}".format(np.std(distances, ddof=1))
-# print "Mean y distance from rotor: {:.4f} ".format(np.mean(end_abs_y_dis)) \
-#       + u"\u00B1" + " {:.4f}".format(np.std(end_abs_y_dis, ddof=1))
-# print "Mean x distance from rotor: {:.4f} ".format(np.mean(end_abs_x_dis)) \
-#       + u"\u00B1" + " {:.4f}".format(np.std(end_abs_x_dis, ddof=1))
+succesful_locations = [x for x in zipped_data if type(x[1][0]) is not str]
+unsuccesful_locations = [x for x in zipped_data if type(x[1][0]) is str]
 
-# plt.figure(figsize=(10, 5))
-# n = plt.hist(distances, bins=100)
-# plt.vlines(np.mean(distances), ymin=0, ymax=max(n[0]), colors='r', linestyles='dashed', linewidths=2)
-# plt.title('Rotor Distance Histogram.')
-# plt.xlabel('Distance from final probe to rotor')
-# plt.ylabel('Counts')
+# Number of ECG statistics
+total_ecg_average = np.mean([x[3] for x in succesful_locations])
+total_ecg_std = np.std([x[3] for x in succesful_locations], ddof=1)
+
+
+# Distance between rotor and final ecg position
+def distance(rotor, ecg):
+    """
+    :param rotor: tuple
+    :param ecg: tuple
+    :return:
+    """
+    x_vector, y_vector = vector_distance(rotor, ecg)
+    return np.sqrt(y_vector**2 + x_vector**2)
+
+
+def vector_distance(rotor, ecg):
+    """
+    :param rotor:
+    :param ecg:
+    :return:
+    """
+    x_vector = ecg[0] - rotor[0]
+    y_vector = ecg[1] - rotor[1]
+    if y_vector > 100:
+        y_vector -= 200
+    if y_vector <= -100:
+        y_vector += 200
+
+    return x_vector, y_vector
+
+
+distances = [distance(x[2], x[1]) for x in succesful_locations]
+end_vector_dis = [vector_distance(x[2], x[1]) for x in succesful_locations]
+end_abs_x_dis = [np.abs(x[0]) for x in end_vector_dis]
+end_abs_y_dis = [np.abs(x[1]) for x in end_vector_dis]
+start_vector_dis = [vector_distance(x[2], x[0]) for x in zipped_data if type(x[1]) is tuple]
+start_x_dis = [x[0] for x in start_vector_dis]
+start_y_dis = [x[1] for x in start_vector_dis]
+
+print '{:.2f}% of rotors found.'.format(len(succesful_locations)/float(len(ECG_start)) * 100)
+print '{:.2f}% Y Loops.'.format(yLoop_number/float(len(ECG_start)) * 100)
+print '{:.2f}% X Loops.'.format(xLoop_number/float(len(ECG_start)) * 100)
+
+print "Average total number of ECGS to find rotor: {:.3f} ".format(total_ecg_average) \
+      + u"\u00B1" + " {:.3f}".format(total_ecg_std)
+print "Mean distance from rotor: {:.3f} ".format(np.mean(distances)) \
+      + u"\u00B1" + " {:.3f}".format(np.std(distances, ddof=1))
+print "Mean y distance from rotor: {:.3f} ".format(np.mean(end_abs_y_dis)) \
+      + u"\u00B1" + " {:.3f}".format(np.std(end_abs_y_dis, ddof=1))
+print "Mean x distance from rotor: {:.3f} ".format(np.mean(end_abs_x_dis)) \
+      + u"\u00B1" + " {:.3f}".format(np.std(end_abs_x_dis, ddof=1))
+
+plt.figure(figsize=(10, 5))
+n = plt.hist(distances, bins=100)
+plt.vlines(np.mean(distances), ymin=0, ymax=max(n[0]), colors='r', linestyles='dashed', linewidths=2)
+plt.title('Rotor Distance Histogram.')
+plt.xlabel('Distance from final probe to rotor')
+plt.ylabel('Counts')
 #
 # fig2, (bx1, bx2) = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
 # yloop_xvec = [(x[2][0] - x[0][0]) for x in zipped_data if x[1] == "Y LOOP"]
@@ -124,14 +110,14 @@ print xLoop_number
 # cx2.set_xlabel('Starting y vector from rotor')
 # cx2.hist(xloop_yvec, bins=50)
 #
-# plt.figure()
-# count_grid = np.zeros((400, 400))
-# for x in end_vector_dis:
-#     count_grid[199 + x[1]][199 + x[0]] += 1
-# plt.imshow(count_grid, interpolation='nearest', origin="lower")
+plt.figure()
+count_grid = np.zeros((200, 200))
+for x in end_vector_dis:
+    count_grid[99 + x[1]][99 + x[0]] += 1
+plt.imshow(count_grid, interpolation='nearest', origin="lower")
 #
 # plt.figure()
 # succ_yecg_num = [x[3][0] for x in zipped_data if type(x[1]) is tuple]
 # plt.scatter(start_x_dis, succ_yecg_num)
-# plt.show()
+plt.show()
 
